a657d0ae9ffaff3dc156d75c2e3ad709d108a154
Jesse Watson <jessedavewatson@gmail.com>
2025-09-08 13:24:26 -0400

    Added delete comment functionality

--- src/components/Comments/Comments.jsx
+++ src/components/Comments/Comments.jsx
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from 'react'
-import { fetchComments } from '../../utils/firebase'
+import { deleteComment, fetchComments } from '../../utils/firebase'
 import { useParams } from 'react-router-dom';
+import { useAuth } from '../../AuthContext';
+import { TrashIcon } from '@radix-ui/react-icons';
 
 function Comments() {
   const [comments, setComments] = useState([])
   const { uid, id } = useParams();
+  const { user } = useAuth();
 
   useEffect( () => {
     const fetch = async () => {
@@ -20,6 +23,15 @@ function Comments() {
     fetch();
   },[])
 
+  const handleDelete = async (comment) => {
+    try {
+      await deleteComment(comment);
+      setComments((prev) => prev.filter(c => c !== comment));
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
   console.log(comments);
   return (
     <>
@@ -31,7 +43,7 @@ function Comments() {
           <span className="text-sm text-zinc-800">{comment.authorName}</span>
           <span className="text-xs text-zinc-400">{comment.createdAt.toDate().toLocaleString()}</span>
         </div>
-
+      {comment.authorId === user.uid && <button onClick={() => handleDelete(comment)} className="hover:bg-zinc-300 p-2 float-right cursor-pointer rounded ml-auto"><TrashIcon className="text-zinc-500"/></button>}
       </div>
       <p className="text-zinc-600">{comment.text}</p>
     </div>
